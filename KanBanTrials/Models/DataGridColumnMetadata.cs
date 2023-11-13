75124f83039af697cd87f8176ad1de7b7de6afb7
cabuk79 <cabuk79@googlemail.com>
2023-11-13 11:31:56 +0000

    working on the conpleted tasks and a DataGrid component

---
+++ KanBanTrials/Models/DataGridColumnMetadata.cs
@@ -0,0 +1,11 @@
+﻿namespace KanBanTrials.Models
+{
+    public class DataGridColumnMetadata
+    {
+        public string Name { get; set; }
+        public string Label { get; set; }
+        public string? FormatString { get; set; }
+        public bool IsSearchable { get; set; }
+        public string? Filter { get; set; }
+    }
+}
